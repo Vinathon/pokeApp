ae0d52986f17fc154aba839fa7e29e52b5ab9296
Vinathon <30534076+Vinathon@users.noreply.github.com>
2021-07-25 13:52:30 +0530

    removed console.log

--- script.js
+++ script.js
@@ -4,8 +4,6 @@ var pokemonDetails = [];
 async function getAllPokemon() {
   let resp = await fetch(pokeUrl);
   let data = await resp.json();
-  console.log(data.results);
-  console.log("ID >>> ",data.id);
   getPokemon(data.results);
 }
 
@@ -16,7 +14,6 @@ async function getPokemon(allPokemon) {
     for (element of allPokemon) {
       let resp = await fetch(element.url);
       let data = await resp.json();
-      console.log("Element ID >>> ", element.id);
       let image = "https://pokeres.bastionbot.org/images/pokemon/"+data.id+".png";
       let name = element.name;
       let weight = data.weight;
@@ -24,7 +21,6 @@ async function getPokemon(allPokemon) {
       let moves = data.moves.map((element) => element.move.name);
 
       pokemonDetails.push({ image, name, weight, abilities, moves });
-      console.log(JSON.stringify(pokemonDetails));
     }
 
     displayData(pokemonDetails);
